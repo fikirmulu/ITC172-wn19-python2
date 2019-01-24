07eac3ecc11fa9188aea0ab1909c88c91238fa95
fikirte <38993144+fikirmulu@users.noreply.github.com>
2019-01-23 16:21:09 -0800

    create a set up

---
+++ Assignment1.php
@@ -0,0 +1,15 @@
+sudo apt install python3-venv
+ls
+cd DjangoProject
+python3 -m venv
+python3 -m venv venv
+ls
+source venv/bin/activate
+pip installdjango
+pip install django
+django-admin startproject pythonclubproject
+ls
+cd pythonclubproj
+cd pythonclubproject
+ls
+python3 manage.py startapp club   
