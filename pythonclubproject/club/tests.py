b9e364c91afd77337bb2c80b3fbc2819808edca9
fikirte <38993144+fikirmulu@users.noreply.github.com>
2019-03-01 15:17:45 -0800

    Added a test file

--- pythonclubproject/club/tests.py
+++ pythonclubproject/club/tests.py
@@ -1,73 +1,31 @@
 
 from django.test import TestCase
-from .models import Product, ProductType, Review
-from .forms import ProductForm
-from datetime import datetime
+from .models import Resource, Meeting, Event
 from django.urls import reverse
 
 # Create your tests here.
 # model tests
 
-class TechProductTest(TestCase):
+class ResourceTest(TestCase):
     def test_stringOutput(self):
-        product=Product(productname='Lenovo Laptop')
-        self.assertEqual(str(product), product.productname)
+        resource=Resource(resourcename='computer')
+        self.assertEqual(str(resource), resource.resourcename)
 
     def test_tablename(self):
-        self.assertEqual(str(Product._meta.db_table), 'product')
+        self.assertEqual(str(Resource._meta.db_table), 'resource')
 
-class ProductTypeTest(TestCase):
+class MeetingTest(TestCase):
     def test_stringOutput(self):
-        techtype=ProductType(typename='Laptop')
-        self.assertEqual(str(techtype), techtype.typename)
+        meeting=Meeting(meetingtitle='salary increment')
+        self.assertEqual(str(meeting), meeting.meetingtitle)
 
     def test_tablename(self):
-        self.assertEqual(str(TechType._meta.db_table), 'producttype')
+        self.assertEqual(str(Meeting._meta.db_table), 'meeting')
 
-class ReviewTest(TestCase):
+class EnentTest(TestCase):
     def test_stringOutput(self):
-        review=Review(reviewtitle='Lenovo Laptop')
-        self.assertEqual(str(review), review.reviewtitle)
+        event=Event(eventtitle='New years')
+        self.assertEqual(str(event), event.eventtitle)
 
     def test_tablename(self):
-        self.assertEqual(str(Review._meta.db_table), 'review')
-
-
-class TestIndex(TestCase):
-    
-    def test_view_url_accessible_by_name(self):
-        response = self.client.get(reverse('index'))
-        self.assertEqual(response.status_code, 200)
-        
-    def test_view_uses_correct_template(self):
-        response = self.client.get(reverse('index'))
-        self.assertEqual(response.status_code, 200)
-        self.assertTemplateUsed(response, 'techapp/index.html')
-
-class TestGetProduct(TestCase):
-
-
-    def test_view_url_exists_at_desired_location(self):
-        response = self.client.get('/techapp/products')
-        self.assertEqual(response.status_code, 200)
-
-    def test_view_url_accessible_by_name(self):
-        response = self.client.get(reverse('getProducts'))
-        self.assertEqual(response.status_code, 200)
-        
-    def test_view_uses_correct_template(self):
-        response = self.client.get(reverse('getProducts'))
-        self.assertEqual(response.status_code, 200)
-        self.assertTemplateUsed(response, 'reviews/products.html')
-
-class New_Product_Form_Test(TestCase):
-
-    # Valid Form Data
-    def test_productForm_is_valid(self):
-        form = ProductForm(data={'productname': "Surface", 'techtype': "laptop", 'user': "steve", 'entrydate': "2018-12-17", 'productURL':"http:microsoft.com", 'productdescription':"lightweight laptop" })
-        self.assertTrue(form.is_valid())
-
-    # Invalid Form Data
-    def test_UserForm_invalid(self):
-        form = ProductForm(data={'productname': "Surface", 'techtype': "laptop", 'user': "steve", 'entrydate': "2018-12-17", 'productURL':"http:microsoft.com", 'productdescription':"lightweight laptop" })
-        self.assertFalse(form.is_valid())
+        self.assertEqual(str(Event._meta.db_table), 'event')
