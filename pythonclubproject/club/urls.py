301d5846fba4b91901b302f400672c9a5a2d56ef
fikirte <38993144+fikirmulu@users.noreply.github.com>
2019-03-01 17:38:08 -0800

    added new resources for the form

--- pythonclubproject/club/urls.py
+++ pythonclubproject/club/urls.py
@@ -1,12 +1,13 @@
 
 from django.urls import path
-from . import views
+from .import views
 
 
 urlpatterns = [
     path('', views.index, name='index'),
     path('clubresources/', views.clubresources, name='resource'),
     path('getmeeting/', views.getmeeting, name='getmeeting'),
-    path('meetingdetail/<int:id>', views.meetingdetail, name='details'),  
+    path('meetingdetail/<int:id>', views.meetingdetail, name='details'),
+    path('newresource/', views.newResource, name='newResource'),  
     
 ]
--- pythonclubproject/club/views.py
+++ pythonclubproject/club/views.py
@@ -1,13 +1,15 @@
 
 from django.shortcuts import render, get_object_or_404
 from.models import Meeting, MeetingMinutes, Resource, Event 
+from.forms import ResourceForm
+
 # Create your views here.
 def index (request):
     return render(request, 'club/index.html')
 # importing all theobjects under the productstype
 def clubresources(request):
     resources_list=Resource.objects.all()
-    return render (request,'club/resources.html', {'resources_list': resources_list})
+    return render(request,'club/resources.html', {'resources_list': resources_list})
 
 def getmeeting(request):
     meeting_list=Meeting.objects.all()
@@ -25,3 +27,15 @@ def clubminutes (request):
 def clubevents (request):
     events_list=Resource.objects.all()
     return render (request,'club/events.html', {'events_list': events_list}) 
+#form view
+def newResource(request):
+    form=ResourceForm
+    if request.method=='POST':
+        form=ResourceForm(request.POST)
+        if form.is_valid():
+            post=form.save(commit=True)
+            post.save()
+            form=ResourceForm()
+    else:
+        form=ResourceForm()
+    return render(request, 'club/newresource.html', {'form':form})
